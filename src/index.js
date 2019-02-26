b3bcfdf4cdf428f36a64b80fff92b0f3d5cd4a07
Elena <Elena@ap-sosnovskayaen.local>
2019-02-26 23:21:10 +0300

    task resolve

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function getLoveTrianglesCount(arr) {
+  var count = 0;
+
+  var currentSpichoneePosition;
+  var currentSpichoneeLover;
+  var currentSpichoneeLoverLoves;
+  var currentSpichoneeLoverLoverLoves;
+
+  for (var i = 0; i < arr.length; i++) {
+    
+      currentSpichoneePosition = i+1;
+      currentSpichoneeLover = arr[i];
+
+      currentSpichoneeLoverLoves = arr[currentSpichoneeLover-1];
+      currentSpichoneeLoverLoverLoves = arr[arr[currentSpichoneeLover-1]-1];
+
+      if(currentSpichoneeLover == currentSpichoneeLoverLoverLoves) continue;
+
+      if(currentSpichoneeLoverLoverLoves == currentSpichoneePosition){
+        count++;
+      }
+    }
+
+    if(count == 0 ) return count;
+    else return count/3;
+    };
